7a05dd281bdf100ce9c6b6619aef95564094e143
marcus.santos <mpsoulbass@gmail.com>
2021-07-30 04:56:30 -0300

    fix: remove uuid param from url

--- stranger_parties/invite/urls.py
+++ stranger_parties/invite/urls.py
@@ -3,4 +3,4 @@ from django.urls import path
 from stranger_parties.invite.api.views import AcceptInvite
 
 app_name = "invite"
-urlpatterns = [path("invite-accept/<str:key>/", AcceptInvite.as_view())]
+urlpatterns = [path("invite-accept/", AcceptInvite.as_view())]
