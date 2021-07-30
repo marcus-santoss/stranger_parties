12c31e829c6f6671b0ff7a2feed766fb0fce9f11
marcus.santos <mpsoulbass@gmail.com>
2021-07-30 04:49:59 -0300

    fix(code): Add more info to Guest and Event detail serializers

--- stranger_parties/invite/api/serializers.py
+++ stranger_parties/invite/api/serializers.py
@@ -1,7 +1,16 @@
-from django.contrib.auth.hashers import make_password
-from rest_framework.serializers import ModelSerializer
+import uuid
+
+from rest_framework.fields import IntegerField
+from rest_framework.serializers import ModelSerializer, ListSerializer
 
 from stranger_parties.invite.models import Guest, Event, Invite
+
+
+# ==================[ GUEST ]=============================================
+class GuestSerializer(ModelSerializer):
+    class Meta:
+        model = Guest
+        fields = ["id", "email", "name", "phone"]
 
 
 # ==================[ EVENT ]=============================================
@@ -11,51 +20,93 @@ class EventSerializer(ModelSerializer):
         fields = ["id", "name", "date_time"]
 
 
-# ==================[ GUEST ]=============================================
-class GuestSerializer(ModelSerializer):
-    def create(self, validated_data):
-        user = super(GuestSerializer, self).create(validated_data)
-        enc_password = make_password(user.password)
-        user.password = enc_password
-        user.save()
-        return user
-
-    def update(self, instance, validated_data):
-        password = None
-
-        if "password" in validated_data.keys():
-            password = validated_data.pop("password")
-
-        user = super(GuestSerializer, self).update(instance, validated_data)
-
-        if password is not None:
-            user.password = make_password(password)
-            user.save()
-
-        return user
+class InviteEventOnlySerializer(ModelSerializer):
+    event = EventSerializer()
 
     class Meta:
-        model = Guest
-        fields = ["id", "email", "first_name", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
+        model = Invite
+        fields = ["event", "confirmation_link"]
 
 
 class GuestDetailSerializer(ModelSerializer):
+    total_invites = IntegerField()
+    attended_events = InviteEventOnlySerializer(many=True)
+    pendent_events = InviteEventOnlySerializer(many=True)
+
     class Meta:
         model = Guest
-        fields = ["id", "email", "first_name", "last_name", "is_active", "last_login"]
-        read_only_fields = ["id", "is_active"]
+        fields = [
+            "id",
+            "email",
+            "name",
+            "phone",
+            "total_invites",
+            "attended_events",
+            "pendent_events",
+        ]
+
+
+class InviteGuestOnlySerializer(ModelSerializer):
+    guest = GuestSerializer()
+
+    class Meta:
+        model = Invite
+        fields = ["guest"]
+
+
+class EventDetailSerializer(ModelSerializer):
+    confirmed_guests = InviteGuestOnlySerializer(many=True)
+    non_confirmed_guests = InviteGuestOnlySerializer(many=True)
+    total_invited = IntegerField()
+
+    class Meta:
+        model = Event
+        fields = [
+            "id",
+            "name",
+            "date_time",
+            "total_invited",
+            "confirmed_guests",
+            "non_confirmed_guests",
+        ]
 
 
 # ==================[ INVITE ]============================================
+
+
 class InviteSerializer(ModelSerializer):
+    guest = ListSerializer(child=IntegerField(), required=True, write_only=True)
+
+    def to_representation(self, instance):
+        event = instance.event
+        return {
+            "message": "All users are invited to event",
+            "event_name": event.name,
+            "event_date": event.date_time.strftime("%d/%m/%Y %H:%M"),
+            "total_invited": event.total_invited,
+        }
+
+    def create(self, validated_data):
+        guest_list = validated_data.pop("guest")
+        validated_data["guest_id"] = guest_list.pop(0)
+        invite = super(InviteSerializer, self).create(validated_data)
+
+        for pk in guest_list:
+            invite.pk = None
+            invite.key = uuid.uuid4()
+            invite.guest_id = pk
+            invite.save()
+
+        return invite
+
     class Meta:
         model = Invite
         fields = ["guest", "event"]
+        extra_kwargs = {"event": {"write_only": True}}
 
 
 class InviteDetailSerializer(ModelSerializer):
-    guest = GuestDetailSerializer()
+    guest = GuestSerializer()
     event = EventSerializer()
 
     class Meta:
@@ -67,7 +118,7 @@ class InviteDetailSerializer(ModelSerializer):
             "guest",
             "event",
             "sent_date",
-            "link",
+            "confirmation_link",
             "is_expired",
             "guest",
             "event",
