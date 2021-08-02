d2c5e4e711e671be921aa588cc976246be27a7c0
Marcus Santos <mpsoulbass@users.noreply.github.com>
2021-08-02 16:07:35 -0300

    Update run_debug.sh

--- binary/run_debug.sh
+++ binary/run_debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker-compose stop backend
-docker container ls -a | grep backend | awk '{ print $1 }' | xargs podman container rm -f
+docker container ls -a | grep backend | awk '{ print $1 }' | xargs docker container rm -f
 docker-compose run --service-ports backend python manage.py runserver 0.0.0.0:8000
